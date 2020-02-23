03552005a5f494f501237224dd6307ca7a2b6a78
my <my@example.com>
2020-02-23 12:35:26 +0000

    Solution

--- services/path.js
+++ services/path.js
@@ -14,6 +14,7 @@ module.exports.getPaths = async (req, res, next) => {
 
 module.exports.createPath = async (req, res, next) => {
 	try {
+		console.log('res is', res);
 		let path = res.input.path;
 		await database('paths').insert({
 			path
@@ -35,6 +36,9 @@ module.exports.deletePaths = async (req, res, next) => {
 };
 
 module.exports.navigate = async (req, res, next) => {
+	function insertAt(path, i, letter) {
+		return path.substring(0, i) + letter + path.substring(i + 1);
+	}
 	try {
 		let id = req.params.id;
 		let result = await database('paths')
@@ -46,34 +50,63 @@ module.exports.navigate = async (req, res, next) => {
 			next(httpError.path_not_found);
 		}
 		// this is the path to navigate
+		console.log("result is", result);
 		let path = result.path;
 		// CODE STARTS HERE
-		let total_r = path.match(/r/g).length;
-		if(path.match(/l/g) != null) {
-			total_r -= path.match(/l/g).length;
-		}
-		let total_d = path.match(/d/g).length;
-		if(path.match(/u/g) != null) {
-			total_d -= path.match(/d/g).length;
-		}
+		/*
+		Count the number of r and d in current path, if found l, or u, minus 1 respectively
+		* */
+		let total_r = 0;
+		let total_d = 0;
 		for (let i = 0; i < path.length; i++) {
-			if (path.charAt(i) == "l") {
-				total_r -= 1;
-			}
-			else if (path.charAt(i) == "u") {
-				total_d -= 1;
-			}
-			if (path.charAt(i) == "?") {
-				if (total_r < 4) {
-					path = path.substring(0, i) + 'r' + path.substring(i + 1);
-					total_r += 1;
-				} else if (total_d < 4) {
-					path = path.substring(0, i) + 'd' + path.substring(i + 1);
-					total_d += 1;
-				} else {
-					path = path.substring(0, i) + 'u' + path.substring(i + 1);
-					total_d -= 1;
-				}
+			if (path.charAt(i) == "r") total_r += 1;
+			else if (path.charAt(i) == "d") total_d += 1;
+			else if (path.charAt(i) == "l") total_r -= 1;
+			else if (path.charAt(i) == "u") total_d -= 1;
+		}
+		/* number of r and d should not exceed 4 in the string at any of the time */
+		let max_r = 0;
+		let max_d = 0;
+		for (let i = 0; i < path.length; i++) {
+			let added = false;
+			switch (path.charAt(i)) {
+				case "l":
+					max_r -= 1;
+					break;
+				case "r":
+					max_r += 1;
+					break;
+				case "u":
+					max_d -= 1;
+					break;
+				case "d":
+					max_d += 1;
+					break;
+				case "?":
+					if (max_r < 4) {
+						if (total_r < 4) {
+							path = insertAt(path, i, 'r');
+							added = true;
+							max_r += 1;
+							total_r += 1;
+						}
+					}
+					if (added == false && max_d < 4) {
+						if(total_d < 4){
+							path = insertAt(path, i, 'd');
+							added = true;
+							max_d += 1;
+							total_d += 1;
+						}
+					}
+					if (added == false) {
+						path = insertAt(path, i, 'u');
+						max_d -= 1;
+						total_d -= 1;
+					}
+					break;
+				default:
+					res.status(400);
 			}
 		}
 		// CODE ENDS HERE
